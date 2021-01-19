1952f6fce0de6caf0676023998bb7ea038147e71
GoastcraftHD <simon.ickler@gmail.com>
2021-01-19 20:42:28 +0100

    removed Fall damage

--- BuildFFA/src/de/GoastcraftHD/BFFA/listerners/PvpListener.java
+++ BuildFFA/src/de/GoastcraftHD/BFFA/listerners/PvpListener.java
@@ -4,6 +4,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 
 import de.GoastcraftHD.BFFA.main.Main;
 
@@ -16,6 +17,10 @@ public class PvpListener implements Listener {
 			return;
 		
 		if (event.getEntity().getLocation().getBlockY() >  Main.getPlugin().getConfig().getInt("PvPHeight")) {
+			event.setCancelled(true);
+		}
+		
+		if (event.getCause().equals(DamageCause.FALL)) {
 			event.setCancelled(true);
 		}
 	}
